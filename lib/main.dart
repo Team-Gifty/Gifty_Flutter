eb584476c9e60f2a72eab78711140f55c7c3bdbc
jihoon <leejh080628@naver.com>
2025-09-29 04:13:38 +0900

    🔖 :: 이미지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gifty_flutter/presentation/onboarding/onboarding_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
         home:
-          Image.asset('../assets/test')
+          OnboardingPage()
     );
   }
 }
--- lib/presentation/onboarding/onboarding_page.dart
+++ lib/presentation/onboarding/onboarding_page.dart
@@ -1,1 +1,30 @@
-ㅣ
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Onboarding'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Welcome to Gifty!'),
+            const SizedBox(height: 20),
+            SvgPicture.asset(
+              'assets/images/qwerimage.svg',
+              width: 200,
+              height: 200,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
